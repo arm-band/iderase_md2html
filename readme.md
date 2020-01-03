2bc61c0d956b84448ccfca519b06da829b0edf8b
Arm=Band <bre4tn.let@gmail.com>
2020-01-03 19:29:00 +0900

    first commit.

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Markdown to HTMLフォーム
+
+MarkdownをHTMLに変換するためのフォームです。  
+巷のMarkdown変換ツールでも良いのですが、h1～h6にidを付与させたくなかったので、marked.jsで変換後、idを除去する処理を追加しました。
+
+## Author
+
+アルム＝バンド
+
+## Published Date
+
+2017/5/18
+
+- ver 1.0
---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+$(function() {
+    //ページトップへ戻る
+    pageTop();
+    
+    //ブラー時発動
+    $("#mdTextarea").on("blur", function(){
+        md2html();
+    });
+});
+
+//ページトップへ戻る
+function pageTop() {
+    var returnPageTop = $(".returnPageTop");
+
+	//下にスクロールしたらヘッダの高さを縮小させる
+	var startPos = 0;
+	$(window).on("scroll", function(){
+		var currentPos = $(this).scrollTop();
+		//ページトップへスクロールして戻る
+		if (currentPos > 400) {
+			returnPageTop.fadeIn();
+		} else {
+			returnPageTop.fadeOut();
+		}
+	});
+
+	//ページトップへスクロールして戻る
+	returnPageTop.on("click", function () {
+		$("body, html").animate({ scrollTop: 0 }, 1000, "swing");
+		return false;
+	});
+}
+
+//ID削除
+function idEraser(str) {
+    var filterdStr = str.replace(/ id="[\w\-]*"/gi, '');
+    
+    return filterdStr;
+}
+
+//md2html同期
+function md2html() {
+    // 要素を取得
+    var input = $("#mdTextarea");
+    var output = $("#htmlTextarea");
+
+    var value = input.val(); //入力テキストエリアから値を取得
+    var md = "";
+    if(value.length > 0) {
+        md = idEraser(marked(value)); //マークダウンから変換し、ID除去フィルタを通す
+    }
+    output.val(md); //アウトプット
+}
+
+window.onload = function() {
+    //初期動作
+    md2html();
+    
+    $("#mdTextarea").select();
+};
